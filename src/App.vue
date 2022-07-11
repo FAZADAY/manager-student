f05ee796b2a7d55eb2d574416b5ea7b42425094c
= <=>
2022-07-11 14:43:41 +0700

    commit to repository

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Manager msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Manager from './components/Manager.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Manager
   }
 }
 </script>
